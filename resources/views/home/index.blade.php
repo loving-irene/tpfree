f945cfbb586129461e773148016fbf500fdad36f
vagrant <vagrant@homestead>
2020-05-11 12:30:05 +0000

    首页模板确定

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    这里是测试
+    @endsection
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            tpfree,免费的图片下载网站
+        </p>
+
+        <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
